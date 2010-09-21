9078d5afd794c928567e6982e32ce4ec803175a7
chrisphe <chris@jadira.co.uk>
2010-09-21 20:44:42 +0000

    Update LocationSafeTimestampType implementation

--- usertype.spi/src/main/java/org/jadira/usertype/dateandtime/shared/temporaltype/LocationSafeTimestampType.java
+++ usertype.spi/src/main/java/org/jadira/usertype/dateandtime/shared/temporaltype/LocationSafeTimestampType.java
@@ -20,63 +20,70 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.sql.Types;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.hibernate.HibernateException;
 
 public class LocationSafeTimestampType extends AbstractLocationSafeUserType {
 
-    private static final long serialVersionUID = -6374256465793823865L;
+	private static final long serialVersionUID = -6374256465793823865L;
 
-    private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    
-    @Override
-    public Timestamp deepCopyNotNull(Object value) {
-        return new Timestamp(((Date) value).getTime());
-    }
+	@Override
+	public Timestamp deepCopyNotNull(Object value) {
+		return fromStringValue(toString(value));
+	}
 
-    @Override
-    public Timestamp get(ResultSet rs, String name) throws SQLException {
-        final Timestamp timestamp = rs.getTimestamp(name, getUtcCalendar());
-        return timestamp;
-    }
+	@Override
+	public Timestamp get(ResultSet rs, String name) throws SQLException {
+		final Timestamp timestamp = rs.getTimestamp(name, getUtcCalendar());
+		return timestamp;
+	}
 
-    @Override
-    public void set(PreparedStatement st, Object value, int index) throws SQLException {
-        if (!(value instanceof Timestamp)) {                
-            value = deepCopy(value);
-        }
-        st.setTimestamp(index, (Timestamp) value, getUtcCalendar());
-    }
+	@Override
+	public void set(PreparedStatement st, Object value, int index) throws SQLException {
+		if (!(value instanceof Timestamp)) {
+			value = deepCopy(value);
+		}
+		st.setTimestamp(index, (Timestamp) value, getUtcCalendar());
+	}
 
-    @Override
-    public Class<Timestamp> returnedClass() {
-        return Timestamp.class;
-    }
+	@Override
+	public Class<Timestamp> returnedClass() {
+		return Timestamp.class;
+	}
 
-    @Override
-    public int sqlType() {
-        return Types.TIMESTAMP;
-    }
-    
-    @Override
-    public Timestamp fromStringValue(String xml) throws HibernateException {
-        try {
-            return new Timestamp( new SimpleDateFormat(TIMESTAMP_FORMAT).parse(xml).getTime() );
-        } catch (ParseException ex) {
-            throw new HibernateException("could not parse XML", ex);
-        }
-    }
-    
-    @Override
-    public String toString(Object value) throws HibernateException {
-        return '\'' + new Timestamp( ( (java.util.Date) value ).getTime() ).toString() + '\'';
-    }
+	@Override
+	public int sqlType() {
+		return Types.TIMESTAMP;
+	}
 
-    public String getName() {
-        return "locationSafeTimestamp";
-    }
+	@Override
+	public Timestamp fromStringValue(String stringValue) throws HibernateException {
+
+		Timestamp ts = null;
+		if (stringValue.length() == 10) {
+			try {
+				long time = java.sql.Date.valueOf(stringValue).getTime();
+				ts = new Timestamp(time);
+			} catch (IllegalArgumentException e) {
+				// Was not a java.sql.Date, let Timestamp handle this value
+			}
+		}
+		if (ts == null) {
+			ts = Timestamp.valueOf(stringValue);
+		}
+
+		return ts;
+	}
+
+	@Override
+	public String toString(Object value) throws HibernateException {
+		if (value instanceof Timestamp) {
+			((Timestamp)value).toString();
+		}
+		return new Timestamp(((java.util.Date) value).getTime()).toString();
+	}
+
+	public String getName() {
+		return "locationSafeTimestamp";
+	}
 }
- 
