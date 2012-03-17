8cc87c4715a25deef22bafa149aca8d6e00bd0b1
chrisphe <chris@jadira.co.uk>
2012-03-17 18:10:06 +0000

    Fix behaviour where a manually set default Joda DateTimeZone is ignored. See http://jira.grails.org/browse/GPJODATIME-25

--- usertype.core/src/main/java/org/jadira/usertype/dateandtime/joda/util/ZoneHelper.java
+++ usertype.core/src/main/java/org/jadira/usertype/dateandtime/joda/util/ZoneHelper.java
@@ -7,27 +7,8 @@ public class ZoneHelper {
     private ZoneHelper() {
     }
     
-    public static DateTimeZone getDefault() {
 
-        DateTimeZone zone = null;
-        try {
-            try {
-                String id = System.getProperty("user.timezone");
-                if (id != null) {
-                    zone = DateTimeZone.forID(id);
-                }
-            } catch (RuntimeException ex) {
-                zone = null;
-            }
-            if (zone == null) {
-                zone = DateTimeZone.forID(java.util.TimeZone.getDefault().getID());
-            }
-        } catch (RuntimeException ex) {
-            zone = null;
-        }
-        if (zone == null) {
-            zone = DateTimeZone.UTC;
-        }
-        return zone;
+    public static DateTimeZone getDefault() {
+        return DateTimeZone.getDefault();
     }
 }
