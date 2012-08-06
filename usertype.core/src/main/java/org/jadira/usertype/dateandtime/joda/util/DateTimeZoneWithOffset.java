0566307c86d45929914991e417848460cae459c2
chrisphe <chris@jadira.co.uk>
2012-08-06 15:42:47 +0000

    Store offset for non-fixed zones to better support DST rollovers.

--- usertype.core/src/main/java/org/jadira/usertype/dateandtime/joda/util/DateTimeZoneWithOffset.java
+++ usertype.core/src/main/java/org/jadira/usertype/dateandtime/joda/util/DateTimeZoneWithOffset.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2012 Christopher Pheby
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.jadira.usertype.dateandtime.joda.util;
 
 import java.io.Serializable;
@@ -44,7 +59,18 @@ public class DateTimeZoneWithOffset implements Serializable {
 			throw new IllegalArgumentException("offsetDateTimeZone must be not null when standardDateTimeZone is not fixed");
 		}
 	}
+
+	public DateTimeZone getStandardDateTimeZone() {
+		return standardDateTimeZone;
+	}
 	
+	public DateTimeZone getOffsetDateTimeZone() {
+		return offsetDateTimeZone;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String toString() {
 		if (offsetDateTimeZone == null) {
@@ -53,12 +79,33 @@ public class DateTimeZoneWithOffset implements Serializable {
 			return standardDateTimeZone.toString() + "{" + offsetDateTimeZone.toString() + "}";
 		}
 	}
-	
-	public DateTimeZone getStandardDateTimeZone() {
-		return standardDateTimeZone;
+		
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(Object obj) {
+        if (obj == null) {
+        	return false;
+        }
+        if (!this.getClass().equals(obj.getClass())) {
+        	return false;
+        }
+       
+        final DateTimeZoneWithOffset obj2 = (DateTimeZoneWithOffset)obj;
+        if (this.standardDateTimeZone.equals(obj2.standardDateTimeZone)
+        	&& offsetDateTimeZone.equals(offsetDateTimeZone)) {
+            return true;
+        }
+        
+        return false;
 	}
-	
-	public DateTimeZone getOffsetDateTimeZone() {
-		return offsetDateTimeZone;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		return standardDateTimeZone.hashCode() * 3 + offsetDateTimeZone.hashCode() * 5;
 	}
 }
