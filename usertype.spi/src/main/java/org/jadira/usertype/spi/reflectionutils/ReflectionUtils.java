92d42192cdef3ea4f367641e582e9673ce7eb6d2
chrisphe <chris@jadira.co.uk>
2012-08-30 14:58:31 +0000

    JDF-20 Enhancements to ReflectionUtils

--- usertype.spi/src/main/java/org/jadira/usertype/spi/reflectionutils/ReflectionUtils.java
+++ usertype.spi/src/main/java/org/jadira/usertype/spi/reflectionutils/ReflectionUtils.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2012 Christopher Pheby
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.jadira.usertype.spi.reflectionutils;
 
 import java.lang.reflect.Constructor;
@@ -73,5 +88,13 @@ public class ReflectionUtils {
 			typeBeingSearched = typeBeingSearched.getSuperclass();
 		}
 		throw new IllegalStateException("Method (" + methodName + ") could not be found in " + clazz.getName());
+	}
+
+	public static Object getFieldValue(Field field, Object target) {
+			try {
+				return field.get(target);
+			} catch (IllegalAccessException e) {
+				throw new IllegalStateException("Unexpected exception accessing field {" + field.getName() + "} of {" + field.getDeclaringClass() + "} : " + e.getMessage());
+			}
 	}
 }
