8522d097e5dc20edb4b4a8aa935467603eec9a4e
Chris Pheby <chris@jadira.co.uk>
2013-11-17 20:21:54 +0000

    Applied copyright and author attribution

--- usertype.core/src/main/java/org/jadira/usertype/dateandtime/joda/PersistentDateTimeAsUtcString.java
+++ usertype.core/src/main/java/org/jadira/usertype/dateandtime/joda/PersistentDateTimeAsUtcString.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2013 Christopher Pheby
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.jadira.usertype.dateandtime.joda;
 
 import org.jadira.usertype.dateandtime.joda.columnmapper.StringColumnDateTimeMapper;
@@ -14,6 +29,7 @@ import org.joda.time.DateTime;
  * This user-type was created to workaround Hibernate's <a href="https://hibernate.atlassian.net/browse/HHH-5574">HHH-5574</a>
  * bug by storing the complete {@code DateTime} data, including the specific time zone, not just the offset (ala ISO 8601), in
  * one single, sortable field.
+ * @author dwijnand
  */
 public class PersistentDateTimeAsUtcString extends AbstractParameterizedUserType<DateTime, String, StringColumnDateTimeMapper> {
 
