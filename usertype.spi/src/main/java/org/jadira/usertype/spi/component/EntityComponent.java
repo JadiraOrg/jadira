da57b4f581195ca0f396f4cf5baa963dd3964217
Chris Pheby <chris@jadira.co.uk>
2014-01-21 01:03:05 +0000

    Compliance with JDK 8's Javadoc lint checking

---
+++ usertype.spi/src/main/java/org/jadira/usertype/spi/component/EntityComponent.java
@@ -0,0 +1,16 @@
+package org.jadira.usertype.spi.component;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({TYPE}) 
+@Retention(RUNTIME)
+public @interface EntityComponent {
+	
+	Class<?> parentType();
+}
