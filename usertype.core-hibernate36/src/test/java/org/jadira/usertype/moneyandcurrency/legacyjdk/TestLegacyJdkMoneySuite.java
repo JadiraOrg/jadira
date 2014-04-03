91fa4c77b4083e860b06b145c275643aa21eb5b1
Chris Pheby <chris@jadira.co.uk>
2014-04-03 19:26:49 +0100

    Removed trailing comma

--- usertype.core-hibernate36/src/test/java/org/jadira/usertype/moneyandcurrency/legacyjdk/TestLegacyJdkMoneySuite.java
+++ usertype.core-hibernate36/src/test/java/org/jadira/usertype/moneyandcurrency/legacyjdk/TestLegacyJdkMoneySuite.java
@@ -11,7 +11,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses(value = { 
-		TestPersistentCurrency.class,
+		TestPersistentCurrency.class
 })
 public class TestLegacyJdkMoneySuite {
 
