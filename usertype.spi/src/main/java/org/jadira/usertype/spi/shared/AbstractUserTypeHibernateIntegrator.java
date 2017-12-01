162af01eb7a8de1e3ab710e502609384412298cf
akarsh-jain <34058090+akarsh-jain@users.noreply.github.com>
2017-12-01 17:38:31 +0530

    Adding ConcurrentHashMapBackedProperties implementation according to new Hibernate commits (hibernate-orm/pull/2076)
    Since hibernate-orm/pull/2076 replaces the implementation of Properties with ConcurrentHashMapBackedProperties for better support of ParameterizedTypes in high concurrency, likewise change is reflected in jadira's hibernate integrator.

--- usertype.spi/src/main/java/org/jadira/usertype/spi/shared/AbstractUserTypeHibernateIntegrator.java
+++ usertype.spi/src/main/java/org/jadira/usertype/spi/shared/AbstractUserTypeHibernateIntegrator.java
@@ -144,7 +144,7 @@ public abstract class AbstractUserTypeHibernateIntegrator implements Integrator 
 	}
 
 	private void configureDefaultProperties(SessionFactoryImplementor sessionFactory, String javaZone, String databaseZone, String seed, String currencyCode, String jdbc42Apis) {
-		Properties properties = new Properties();
+		ConcurrentHashMapBackedProperties properties = new ConcurrentHashMapBackedProperties();
 		if (databaseZone != null) { properties.put("databaseZone", databaseZone); }
 		if (javaZone != null) { properties.put("javaZone", javaZone); }
 		if (seed != null) { properties.put("seed", seed); }
