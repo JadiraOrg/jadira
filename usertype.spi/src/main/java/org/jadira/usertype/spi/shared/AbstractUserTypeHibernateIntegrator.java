de264ee6d3bb5618f46b644c26ff7f0ec7170476
Chris Pheby <chris@jadira.co.uk>
2018-01-15 15:39:00 +0000

    After consideration, backed out the change from #66

--- usertype.spi/src/main/java/org/jadira/usertype/spi/shared/AbstractUserTypeHibernateIntegrator.java
+++ usertype.spi/src/main/java/org/jadira/usertype/spi/shared/AbstractUserTypeHibernateIntegrator.java
@@ -30,7 +30,6 @@ import org.hibernate.service.spi.SessionFactoryServiceRegistry;
 import org.hibernate.usertype.CompositeUserType;
 import org.hibernate.usertype.UserType;
 import org.jadira.usertype.spi.utils.runtime.JavaVersion;
-import org.hibernate.type.ConcurrentHashMapBackedProperties;
 
 public abstract class AbstractUserTypeHibernateIntegrator implements Integrator {
 
@@ -145,7 +144,7 @@ public abstract class AbstractUserTypeHibernateIntegrator implements Integrator 
 	}
 
 	private void configureDefaultProperties(SessionFactoryImplementor sessionFactory, String javaZone, String databaseZone, String seed, String currencyCode, String jdbc42Apis) {
-		ConcurrentHashMapBackedProperties properties = new ConcurrentHashMapBackedProperties();
+		Properties properties = new Properties();
 		if (databaseZone != null) { properties.put("databaseZone", databaseZone); }
 		if (javaZone != null) { properties.put("javaZone", javaZone); }
 		if (seed != null) { properties.put("seed", seed); }
